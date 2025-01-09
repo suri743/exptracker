f3071cf9622ba2ef52ac0715da25997560da90bc
Surish Ayya <suresh.ayya@sap.com>
2025-01-09 16:56:13 +0530

    Initial commit

---
+++ src/main/java/com/suri/exptracker/ExptrackerApplication.java
@@ -0,0 +1,13 @@
+package com.suri.exptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExptrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExptrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suri/exptracker/ExptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suri.exptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
